47a6986bbb84853bdc49a541255b1a2f3e41be82
Dimagb <info@geekbrains.ru>
2020-09-19 09:03:58 +0300

    hm1

--- src/main/java/ru/geekbrains/java1/homework/hm1.java
+++ src/main/java/ru/geekbrains/java1/homework/hm1.java
@@ -16,7 +16,6 @@ public class hm1 {
 
         }
     }
-
     public static void isPositiveOrNegative(int x) {
         if (x >= 0) {
             System.out.println("Число " + x + " Положительное");
